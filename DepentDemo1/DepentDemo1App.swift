1a1bdcb254105ca2733a7ec4ff8f92bcd762e7e4
Akramjon <akramjon0175@gmail.com>
2023-09-12 08:55:24 -0700

    Initial Commit

---
+++ DepentDemo1/DepentDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  DepentDemo1App.swift
+//  DepentDemo1
+//
+//  Created by Akramjon on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DepentDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
